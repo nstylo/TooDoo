206f445b1dedc03717f4c9726bd1f88cbb122a65
Aerial <matas234@gmail.com>
2019-07-25 17:42:51 +0200

    Add auth to Django and permission control
    Every entry is attached with an "owner" that's linked to the User table, users can only see todo items once logged in,
    the admin user can see all entries, non authenticated users don't see anything. The admin user is the only one that can
    see the User api endpoint.

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Todo(models.Model):
     """The ToDo app database"""
 
+    owner = models.ForeignKey('auth.User', related_name='todo', on_delete=models.CASCADE)
     entry = models.CharField(max_length=80)
     done = models.BooleanField(default=False)
     dateTime = models.DateTimeField(auto_now=False, auto_now_add=True)
